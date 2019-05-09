17d13bbf581cebaec200b9370cb588235d6dcc9d
deno101 <dennizwarui@gmail.com>
2019-05-09 21:58:15 +0300

    create database & make migrations

---
+++ databaseinfo.sql
@@ -0,0 +1,10 @@
+CREATE database matprog;
+
+-- default password == 'denniz'
+--         user == 'deno101'
+--
+-- can change this is Safari_sys/settings.py
+--         DATABASES = {
+--                 'USER': ''
+--                 'PASSWORD': ''
+--         }
